d13b19826f1ddcf83b1831fb0490f9f94b23669d
Gerald Versluis <gerald.versluis@microsoft.com>
2025-10-31 12:08:10 +0100

    Add MAUI Aspire integration

--- src/SentenceStudio.AppHost/AppHost.cs
+++ src/SentenceStudio.AppHost/AppHost.cs
@@ -4,8 +4,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var webapi = builder.AddProject<SentenceStudio_Web>("web");
 
-// builder.AddProject<SentenceStudio>("app")
-//     .WithReference(webapi)
-//     .ExcludeFromManifest();
+var mauiapp = builder.AddMauiProject("mauiapp", "../SentenceStudio/SentenceStudio.csproj");
+
+mauiapp.AddMacCatalystDevice()
+    .WithReference(webapi);
 
 builder.Build().Run();
--- src/SentenceStudio.ServiceDefaults/Extensions.cs
+++ src/SentenceStudio.ServiceDefaults/Extensions.cs
@@ -54,12 +54,18 @@ public static class Extensions
 
         builder.Services.AddOpenTelemetry()
             .WithMetrics(metrics =>
-            {                
+            {
+                // Uncomment the following line to enable reporting metrics coming from the .NET MAUI SDK, this might cause a lot of added telemetry
+                //metrics.AddMeter("Microsoft.Maui");
+                
                 metrics.AddHttpClientInstrumentation()
                     .AddRuntimeInstrumentation();
             })
             .WithTracing(tracing =>
             {
+                // Uncomment the following line to enable reporting tracing coming from the .NET MAUI SDK, this might cause a lot of added telemetry
+                //tracing.AddSource("Microsoft.Maui");
+
                 tracing.AddSource(builder.Environment.ApplicationName)
                     // Uncomment the following line to enable gRPC instrumentation (requires the OpenTelemetry.Instrumentation.GrpcNetClient package)
                     //.AddGrpcClientInstrumentation()
@@ -67,28 +73,6 @@ public static class Extensions
             });
 
         builder.AddOpenTelemetryExporters();
-
-        return builder;
-    }
-
-    /// <summary>
-    /// Disables the security check for ASP.NET development certificates.
-    /// This should only be used in development environments.
-    /// </summary>
-    [Obsolete("This method should only be used for development purposes.", true)]
-    public static IHttpClientBuilder DisableDevCertSecurityCheck(this IHttpClientBuilder builder)
-    {
-        builder.ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
-        {
-            ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
-            {
-                if (cert is not null && cert.Issuer.Equals("CN=localhost", StringComparison.OrdinalIgnoreCase))
-                {
-                    return true;
-                }
-                return errors == System.Net.Security.SslPolicyErrors.None;
-            }
-        });
 
         return builder;
     }
@@ -111,15 +95,6 @@ public static class Extensions
         {
             builder.Services.AddOpenTelemetry().UseOtlpExporter();
         }
-
-        // TODO MAUI: this code comes from the Aspire service defaults, we will want to check if this works for us and if yes
-        // how integration works for us because the AspNetCore package cannot be added to a MAUI project and we can't read the connection string like this
-        // Uncomment the following lines to enable the Azure Monitor exporter (requires the Azure.Monitor.OpenTelemetry.AspNetCore package)
-        //if (!string.IsNullOrEmpty(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]))
-        //{
-        //    builder.Services.AddOpenTelemetry()
-        //       .UseAzureMonitor();
-        //}
 
         return builder;
     }
