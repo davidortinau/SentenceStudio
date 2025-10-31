df7fa9b307ed85d1c4187118ccd6c101499de49e
Gerald Versluis <gerald.versluis@microsoft.com>
2025-10-31 13:35:16 +0100

    Add environment variables and Windows

--- src/SentenceStudio.AppHost/AppHost.cs
+++ src/SentenceStudio.AppHost/AppHost.cs
@@ -2,11 +2,24 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var openaikey = builder.AddParameter("openaikey", secret: true);
+var syncfusionkey = builder.AddParameter("syncfusionkey", secret: true);
+var elevenlabskey = builder.AddParameter("elevenlabskey", secret: true);
+
 var webapi = builder.AddProject<SentenceStudio_Web>("web");
 
 var mauiapp = builder.AddMauiProject("mauiapp", "../SentenceStudio/SentenceStudio.csproj");
 
 mauiapp.AddMacCatalystDevice()
+    .WithEnvironment("SyncfusionKey", syncfusionkey)
+    .WithEnvironment("AI__OpenAI__ApiKey", openaikey)
+    .WithEnvironment("ElevenLabsKey", elevenlabskey)
+    .WithReference(webapi);
+
+mauiapp.AddWindowsDevice()
+    .WithEnvironment("SyncfusionKey", syncfusionkey)
+    .WithEnvironment("AI__OpenAI__ApiKey", openaikey)
+    .WithEnvironment("ElevenLabsKey", elevenlabskey)
     .WithReference(webapi);
 
 builder.Build().Run();
